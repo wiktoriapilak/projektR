f966a8d29dce714355ee683bcd0a51cc92acad4f
wiktoriapilak <163404868+wiktoriapilak@users.noreply.github.com>
2024-05-03 18:36:16 +0200

    Add files via upload

--- ggtrends/R/data_processing.R
+++ ggtrends/R/data_processing.R
@@ -1,1 +1,20 @@
-#formatowanie danych
+importFile<- function(path){
+  newTable = read.csv(path, skip=2)
+  return(newTable)
+}
+
+doTimeTable <- function (rawFile){
+  rawFile$year = as.numeric(substring(rawFile[,1],1,4))
+  rawFile$month = as.numeric(substring(rawFile[,1],6,7))
+  rawFile = rawFile[, -1]
+  names(rawFile)[1] <- "amount"
+  TimeTable = na.omit(rawFile)
+  return(TimeTable)
+}
+
+doRegionTable <- function (rawFile){
+  names(rawFile)[1] <- "country"
+  names(rawFile)[2] <- "amount"
+  RegionTable = na.omit(rawFile)
+  return(RegionTable)
+}
