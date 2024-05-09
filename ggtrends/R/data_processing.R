6f2fbc92606f17660e4e37351e51639886600673
wiktoriapilak <163404868+wiktoriapilak@users.noreply.github.com>
2024-05-09 08:36:37 +0200

    Update data_processing.R

--- ggtrends/R/data_processing.R
+++ ggtrends/R/data_processing.R
@@ -1,20 +1,23 @@
 importFile<- function(path){
   newTable = read.csv(path, skip=2)
+  newTable = na.omit(newTable)
   return(newTable)
 }
 
-doTimeTable <- function (rawFile){
-  rawFile$year = as.numeric(substring(rawFile[,1],1,4))
-  rawFile$month = as.numeric(substring(rawFile[,1],6,7))
-  rawFile = rawFile[, -1]
-  names(rawFile)[1] <- "amount"
-  TimeTable = na.omit(rawFile)
-  return(TimeTable)
+doDateTable <-function (rawFile){
+  names(rawFile)[1] <- "week"
+  names(rawFile)[2] <- "amount"
+  rawFile[,1]=as.Date(rawFile[,1])
+  dateTable=rawFile
+  return(dateTable)
 }
 
-doRegionTable <- function (rawFile){
-  names(rawFile)[1] <- "country"
-  names(rawFile)[2] <- "amount"
-  RegionTable = na.omit(rawFile)
-  return(RegionTable)
+doSplitTable <- function (rawFile){
+  rawFile$year = as.numeric(substring(rawFile[,1],1,4))
+  rawFile$month = as.numeric(substring(rawFile[,1],6,7))
+  rawFile$day = as.numeric(substring(rawFile[,1],9,10))
+  rawFile = rawFile[, -1]
+  names(rawFile)[1] <- "amount"
+  splitTable <- rawFile[, c(2:ncol(rawFile), 1)]
+  return(timeTable)
 }
