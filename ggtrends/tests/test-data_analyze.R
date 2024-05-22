b612f07fdf5363f0f88b92c66575c9e3f3f8fda0
SvenjaMiklaszewska <137147531+SvenjaMiklaszewska@users.noreply.github.com>
2024-05-22 19:05:54 +0200

    Update test-data_analyze.R
    Mediana, odchyl standardowe itp

--- ggtrends/tests/test-data_analyze.R
+++ ggtrends/tests/test-data_analyze.R
@@ -34,3 +34,66 @@ combined_df <- combined_df %>%
   # Przykład użycia funkcji
   rodzaj <- "baggy_jeans"
   tworz_wykres_sezonowy(statystyki_sezonowe, rodzaj)
+
+# Analiza statystyczna tj. mediana, średnia, odchylenie standardowe
+statystyki_opisowe <- combined_df %>%
+    group_by(przedzial_lat, rodzaj) %>%
+    summarize(
+      srednia = mean(wartosc, na.rm = TRUE),
+      mediana = median(wartosc, na.rm = TRUE),
+      odchylenie_standardowe = sd(wartosc, na.rm = TRUE),
+      n = n()
+    ) %>%
+    ungroup()
+  statystyki_opisowe = statystyki_opisowe[-c(31:33),]
+  
+# Wykres średnich
+  ggplot(statystyki_opisowe, aes(x = przedzial_lat, y = srednia, color = rodzaj, group = rodzaj)) +
+    geom_line() +
+    geom_point() +
+    labs(title = "Średnie wartości dla różnych rodzajów w poszczególnych przedziałach czasowych",
+         x = "Przedział czasowy",
+         y = "Średnia wartość") +
+    theme_minimal() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+  
+  # Wykres median
+  ggplot(statystyki_opisowe, aes(x = przedzial_lat, y = mediana, color = rodzaj, group = rodzaj)) +
+    geom_line() +
+    geom_point() +
+    labs(title = "Median wartości dla różnych rodzajów w poszczególnych przedziałach czasowych",
+         x = "Przedział czasowy",
+         y = "Mediana wartości") +
+    theme_minimal() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+  
+  # Wykres odchylenia standardowego
+  ggplot(statystyki_opisowe, aes(x = przedzial_lat, y = odchylenie_standardowe, color = rodzaj, group = rodzaj)) +
+    geom_line() +
+    geom_point() +
+    labs(title = "Odchylenie standardowe wartości dla różnych rodzajów w poszczególnych przedziałach czasowych",
+         x = "Przedział czasowy",
+         y = "Odchylenie standardowe wartości") +
+    theme_minimal() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+    
+# funkcja usprawniająca bardziej do shiny
+  create_plot <- function(statistics_df, stat_col, stat_name) {
+    ggplot(statistics_df, aes_string(x = "przedzial_lat", y = stat_col, color = "rodzaj", group = "rodzaj")) +
+      geom_line() +
+      geom_point() +
+      labs(title = paste(stat_name, "wartości dla różnych rodzajów w poszczególnych przedziałach czasowych"),
+           x = "Przedział czasowy",
+           y = paste(stat_name, "wartość")) +
+      theme_minimal() +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  }
+  
+  # Przykładowe wywołania funkcji dla różnych statystyk
+  create_plot(statystyki_opisowe, "srednia", "Średnie")
+  create_plot(statystyki_opisowe, "mediana", "Median")
+  create_plot(statystyki_opisowe, "odchylenie_standardowe", "Odchylenie standardowe")
+  
