14c5491f74b1b7c4f54f4116e59d64a1e894bce9
wiktoriapilak <163404868+wiktoriapilak@users.noreply.github.com>
2024-05-26 16:03:24 +0200

    rozdzielenie funkcji do odpowiednich plikow

--- ggtrends/R/data_processing.R
+++ ggtrends/R/data_processing.R
@@ -9,11 +9,11 @@ read_all_files <- function(folder_path) {
 
 process_data <- function(data_list, files) {
   processed_list <- mapply(function(df, file) {
-    type_value <- file_path_sans_ext(basename(file))
+    rodzaj_value <- file_path_sans_ext(basename(file))
     df <- df %>%
       slice(-1, -2) %>%
       mutate(
-        type = type_value,
+        value = rodzaj_value,
         week = as.Date(V1, format="%Y-%m-%d"),
         year = as.numeric(format(week, "%Y")),
         month = format(week, "%m"),
@@ -25,15 +25,33 @@ process_data <- function(data_list, files) {
           right = TRUE
         )
       ) %>%
+      mutate(month = as.numeric(month)) %>%
+      mutate(
+        season = case_when(
+          month %in% c(12, 1, 2) ~ "Zima",
+          month %in% c(3, 4, 5) ~ "Wiosna",
+          month %in% c(6, 7, 8) ~ "Lato",
+          month %in% c(9, 10, 11) ~ "Jesień"
+        )
+      ) %>%
       select(-V1)
+
     return(df)
   }, data_list, files, SIMPLIFY = FALSE)
 
   return(processed_list)
 }
 
+
 combine_data_frames <- function(processed_list) {
   combined_df <- bind_rows(processed_list)
-  names(combined_df) <- c("value", "type", "week", "year", "month", "day", "range_of_years")
+  names(combined_df) <- c("value", "type", "week", "year", "month", "day", "range_of_years","season")
   return(combined_df)
 }
+
+
+#przykladowe uzycie
+folder_path <- "C:/Users/Wiktoria/Desktop/ProjektR/ggtrends/data"
+all_data <- read_all_files(folder_path)
+processed_data <- process_data(all_data$data_list, all_data$files)
+combined_df = combine_data_frames(processed_data)
