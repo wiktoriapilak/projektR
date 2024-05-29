9630abe4f8b0fb7aa37973207dd3b9eb9e3d01f5
wiktoriapilak <163404868+wiktoriapilak@users.noreply.github.com>
2024-05-29 12:50:48 +0200

    Update data_processing.R

--- ggtrends/R/data_processing.R
+++ ggtrends/R/data_processing.R
@@ -7,51 +7,56 @@ read_all_files <- function(folder_path) {
   return(list(data_list = data_list, files = files))
 }
 
-process_data <- function(data_list, files) {
-  processed_list <- mapply(function(df, file) {
-    rodzaj_value <- file_path_sans_ext(basename(file))
-    df <- df %>%
-      slice(-1, -2) %>%
-      mutate(
-        value = rodzaj_value,
-        week = as.Date(V1, format="%Y-%m-%d"),
-        year = as.numeric(format(week, "%Y")),
-        month = format(week, "%m"),
-        day = format(week, "%d"),
-        range_of_years = cut(
-          year,
-          breaks = seq(2003, 2024, by = 3),
-          labels = c("2004-2006", "2007-2009", "2010-2012", "2013-2015", "2016-2018", "2019-2021", "2022-2024"),
-          right = TRUE
-        )
-      ) %>%
-      mutate(month = as.numeric(month)) %>%
-      mutate(
-        season = case_when(
-          month %in% c(12, 1, 2) ~ "Zima",
-          month %in% c(3, 4, 5) ~ "Wiosna",
-          month %in% c(6, 7, 8) ~ "Lato",
-          month %in% c(9, 10, 11) ~ "Jesień"
-        )
-      ) %>%
-      select(-V1)
-
-    return(df)
-  }, data_list, files, SIMPLIFY = FALSE)
-
-  return(processed_list)
+split_date <- function(data) {
+  data$year <- as.integer(substr(data$V1, 1, 4))
+  data$month <- as.integer(substr(data$V1, 6, 7))
+  data <- data[, -1]
+  return(data)
 }
 
+add_year_range_column <- function(data) {
+  data$range_of_years <- cut(
+    data$year,
+    breaks = seq(2003, 2024, by = 3),
+    labels = c("2004-2006", "2007-2009", "2010-2012", "2013-2015", "2016-2018", "2019-2021", "2022-2024"),
+    right = TRUE,
+  )
+  return(data)
+}
 
-combine_data_frames <- function(processed_list) {
-  combined_df <- bind_rows(processed_list)
-  names(combined_df) <- c("value", "type", "week", "year", "month", "day", "range_of_years","season")
+add_season_column <- function(data) {
+  data$season <- cut(
+    data$month,
+    breaks = c(0, 3, 6, 9, 12),
+    labels = c("Spring", "Summer", "Autumn", "Winter"),
+    right = TRUE,
+    include.lowest = TRUE
+  )
+  return(data)
+}
+
+process_df <- function(data_list) {
+  processed_df <- lapply(data_list, function(data) {
+    rodzaj_value <- gsub(":.*", "", data[1, 2])
+    data$type <- rodzaj_value
+    data <- data[-c(1:2), ]
+    data <- split_date(data)
+    data <- add_year_range_column(data)
+    data <- add_season_column(data)
+    colnames(data)[1] <- "value"
+    data$type <- rodzaj_value
+    return(data)
+  })
+  return(processed_df)
+}
+
+combine_data_frames <- function(processed_df) {
+  combined_df <- bind_rows(processed_df)
   return(combined_df)
 }
-
 
 #przykladowe uzycie
 folder_path <- "C:/Users/Wiktoria/Desktop/ProjektR/ggtrends/data"
 all_data <- read_all_files(folder_path)
-processed_data <- process_data(all_data$data_list, all_data$files)
+processed_data <- process_df(all_data$data_list)
 combined_df = combine_data_frames(processed_data)
