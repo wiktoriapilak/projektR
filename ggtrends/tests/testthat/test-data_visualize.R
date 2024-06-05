93cd7bd6822bc24e3cac39f8741fbca5f3975ff9
wiktoriapilak <163404868+wiktoriapilak@users.noreply.github.com>
2024-06-05 18:35:32 +0200

    Update test-data_visualize.R

--- ggtrends/tests/testthat/test-data_visualize.R
+++ ggtrends/tests/testthat/test-data_visualize.R
@@ -4,25 +4,85 @@ test_that("draw_season_plot function works properly", {
     mean_value = c(1, 2, 3, 4),
     type = rep("A", 4)
   )
-  plot <- draw_season_plot(data, "A")
+  plot <- draw_season_plot(data)
   expect_true("ggplot" %in% class(plot))
 })
 
-test_that("create_plot function works properly", {
+test_that("create_stat_plot function works properly", {
   statistics_df <- data.frame(
     range_of_years = c("2004-2006", "2007-2009", "2010-2012"),
     type = c("A", "A", "A"),
     mean_value = c(1, 2, 3)
   )
-  plot <- create_plot(statistics_df, "mean_value", "Średnia")
+  plot <- create_stat_plot(statistics_df, "mean_value", "Średnia")
   expect_true("ggplot" %in% class(plot))
 })
 
-test_that("draw_trend_line function works properly", {
+test_that("draw_tao function works properly", {
   data <- data.frame(
-    x = c(1, 2, 3, 4, 5),
-    y = c(2, 4, 6, 8, 10)
+    year = c(1, 2, 3, 4, 5),
+    value = c(2, 4, 6, 8, 10)
   )
-  plot <- draw_trend_line(data, "x", "y")
+  plot <- draw_tao(data)
+  expect_true("ggplot" %in% class(plot))
+})
+
+test_that("draw_candlestick_plot function works properly", {
+  data <- data.frame(
+    range_of_years = c("2004-2006", "2007-2009", "2010-2012"),
+    value = c(1, 2, 3),
+    type = c("A", "A", "A")
+  )
+  plot <- draw_candlestick_plot(data)
+  expect_true("ggplot" %in% class(plot))
+})
+
+test_that("draw_scatter_with_trend_plot function works properly", {
+  data <- data.frame(
+    year = c(1, 2, 3, 4, 5),
+    value = c(2, 4, 6, 8, 10),
+    type = c("A", "A", "B", "B", "B")
+  )
+  plot <- draw_scatter_with_trend_plot(data)
+  expect_true("ggplot" %in% class(plot))
+})
+
+test_that("draw_time_series_plot function works properly", {
+  data <- data.frame(
+    year = c(2004, 2005, 2006, 2007, 2008),
+    value = c(1, 2, 3, 4, 5),
+    type = c("A", "A", "B", "B", "B")
+  )
+  plot <- draw_time_series_plot(data)
+  expect_true("ggplot" %in% class(plot))
+})
+
+test_that("draw_scatter_plot function works properly", {
+  data <- data.frame(
+    range_of_years = c("2004-2006", "2007-2009", "2010-2012"),
+    value = c(1, 2, 3),
+    type = c("A", "A", "A")
+  )
+  plot <- draw_scatter_plot(data)
+  expect_true("ggplot" %in% class(plot))
+})
+
+test_that("draw_mean_plot function works properly", {
+  data <- data.frame(
+    range_of_years = c("2004-2006", "2007-2009", "2010-2012"),
+    mean_value = c(1, 2, 3),
+    type = c("A", "A", "A")
+  )
+  plot <- draw_mean_plot(data, data)
+  expect_true("ggplot" %in% class(plot))
+})
+
+test_that("draw_value_distribution_plot function works properly", {
+  data <- data.frame(
+    range_of_years = c("2004-2006", "2007-2009", "2010-2012"),
+    value = c(1, 2, 3),
+    type = c("A", "A", "A")
+  )
+  plot <- draw_value_distribution_plot(data)
   expect_true("ggplot" %in% class(plot))
 })
