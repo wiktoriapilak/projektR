d5e368748639a1f3d78ed03e77994e3ff48b8c6f
wiktoriapilak <163404868+wiktoriapilak@users.noreply.github.com>
2024-06-05 19:46:27 +0200

    Update data_analyze.R

--- ggtrends/R/data_analyze.R
+++ ggtrends/R/data_analyze.R
@@ -1,4 +1,10 @@
-# Analiza statystyczna tj. mediana, średnia, odchylenie standardowe
+#' Calculate statistical parameters (mean, median, standard deviation)
+#'
+#' @param df A data frame containing the data.
+#' @param parameter The parameter to group by.
+#' @return A data frame with calculated statistics.
+#' @importFrom dplyr mutate group_by summarize
+#' @export
 calculate_statistics <- function(df, parameter) {
   statistics <- df %>%
     mutate(value = as.numeric(value)) %>%
@@ -13,18 +19,27 @@ calculate_statistics <- function(df, parameter) {
   return(statistics)
 }
 
-# Funkcja do obliczania współczynnika Tau Kendalla dla dwóch zmiennych
+#' Calculate Kendall's Tau coefficient for two variables
+#'
+#' @param data A data frame containing `year` and `value` columns.
+#' @return The Kendall's Tau coefficient.
+#' @importFrom stats cor
+#' @export
 count_tau_kendall <- function(data) {
   cleaned_data <- na.omit(data[, c("year", "value")])
   tau_kendall <- cor(cleaned_data$year, cleaned_data$value, method = "kendall")
   return(tau_kendall)
 }
 
-# funkcja pomocnicza do sredniej wartosci
+#' Calculate mean results for the data
+#'
+#' @param data A data frame containing the data.
+#' @return A data frame with mean results.
+#' @importFrom dplyr group_by summarize
+#' @export
 calculate_mean_results <- function(data) {
   mean_results <- data %>%
     group_by(range_of_years, type) %>%
     summarize(mean_value = mean(value, na.rm = TRUE))
   return(mean_results)
 }
-
